92c6cbf86fe92208a5c98be92a4b2ff70c0d6fe7
Bala Chadramouli, Ketharaju <ketharaju.balachadramouli@lowes.com>
2023-02-24 08:24:21 +0530

    My first commit

---
+++ src/main/java/com/springmongo/springmongoAggr/SpringmongoAggrApplication.java
@@ -0,0 +1,13 @@
+package com.springmongo.springmongoAggr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmongoAggrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmongoAggrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springmongo/springmongoAggr/SpringmongoAggrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmongo.springmongoAggr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmongoAggrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
